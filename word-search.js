eecc21b7c29b099c0a47bdf6f129d7dde5c2ff2a
Dave Harms <daveharmswebdev@gmail.com>
2016-08-26 09:33:12 -0500

    instantiated read write transform and took an argument

--- word-search.js
+++ word-search.js
@@ -1,1 +1,11 @@
-console.log('hey');
+'use strict'
+
+const { Readable, Writable, Transform } = require('stream')
+const fs = require('fs')
+const readStream = Readable()
+const writeStream = Writable()
+const transStream = Transform()
+
+const [,,...args] = process.argv
+
+console.log(args[0])
