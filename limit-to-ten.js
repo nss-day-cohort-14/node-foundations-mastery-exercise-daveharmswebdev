6aef379b36bbb72b16268028e394f2af94687e21
Dave Harms <daveharmswebdev@gmail.com>
2016-08-26 09:58:20 -0500

    created limit-to-ten module

---
+++ limit-to-ten.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = function(data) {
+  data = data.split('\n')
+  return data.length
+}
--- word-search.js
+++ word-search.js
@@ -5,12 +5,19 @@ const fs = require('fs')
 const readStream = Readable()
 const writeStream = Writable()
 const transStream = Transform()
-
+const limitToTen = require('./limit-to-ten')
 const [,,...args] = process.argv
-
 
 if (typeof args[0] !== 'undefined') {
   console.log('yeah', args[0])
+  fs.readFile('words/en.txt', 'utf8', (err,data) => {
+    if (err) throw err
+    rtw(data)
+  })
 } else {
   console.log('Usage: word-search.js [searchterm]')
+}
+
+function rtw(data) {
+  console.log(limitToTen(data))
 }
