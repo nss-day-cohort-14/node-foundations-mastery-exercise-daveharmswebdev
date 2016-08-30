c0025688cf1b4b2a15e91552879ec4b7af6efffb
Dave Harms <daveharmswebdev@gmail.com>
2016-08-29 21:59:43 -0500

    removed commented out code

--- word-search.js
+++ word-search.js
@@ -8,20 +8,20 @@ const transStream = require('./limit-to-ten')
 // console.log(limitToTen)S
 const [,,...args] = process.argv
 
-if (typeof args[0] !== 'undefined') {
+function search() {
   const searchTerm = args[0].toLowerCase()
   const readStream = fs.createReadStream('words/en.txt')
-    .pipe(es.split())
-    .pipe(es.map((line, cb) => {
-      if (line.toLowerCase().slice(0,searchTerm.length) !== searchTerm) {
-        line = ''
-      } else {
-        line += '\n'
-      }
-      cb(null, line)
-    }))
-    .pipe(transStream)
-    .pipe(writeStream)
+  .pipe(es.split())
+  .pipe(es.map((line, cb) => {
+    if (line.toLowerCase().slice(0,searchTerm.length) !== searchTerm) {
+      line = ''
+    } else {
+      line += '\n'
+    }
+    cb(null, line)
+  }))
+  .pipe(transStream)
+  .pipe(writeStream)
 
   // writestream code
   writeStream._write = (buffer, _, cb) => {
@@ -29,7 +29,21 @@ if (typeof args[0] !== 'undefined') {
     process.stdout.write(`${output}`)
     cb()
   }
-  // readStream.pipe(transStream).pipe(writeStream)
+}
+
+if (typeof args[0] !== 'undefined') {
+  search()
 } else {
-  console.log('Usage: word-search.js [searchterm]')
+  process.stdin.setEncoding('utf8')
+  process.stdin.once('readable', () => {
+    let chunk = process.stdin.read()
+    if (typeof chunk === 'string') {
+      process.stdout.write(chunk)
+      if (typeof args[0] !== 'undefined') {
+        search()
+      }
+    } else {
+      console.log('Usage: word-search.js [searchterm]')
+    }
+  })
 }
